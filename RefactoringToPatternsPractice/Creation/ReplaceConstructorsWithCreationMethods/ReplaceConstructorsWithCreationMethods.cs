7e27a9870f517014b2d9e7d331d65b691e0c1004
Bryan <d58526@gmail.com>
2017-12-27 15:14:16 +0800

    ReplaceConstructorsWithCreationMethods - After

--- RefactoringToPatternsPractice/Creation/ReplaceConstructorsWithCreationMethods/ReplaceConstructorsWithCreationMethods.cs
+++ RefactoringToPatternsPractice/Creation/ReplaceConstructorsWithCreationMethods/ReplaceConstructorsWithCreationMethods.cs
@@ -8,6 +8,26 @@ namespace RefactoringToPatternsPractice.Creation.ReplaceConstructorsWithCreation
 {
     public class ReplaceConstructorsWithCreationMethods
     {
+        public void Sample()
+        {
+            var commitment = 0.00;
+            var riskRating = 0;
+            DateTime maturity = new DateTime();
+            Loan termLoan = Loan.CreateTermLoan(commitment, riskRating, maturity);
+
+            var riskAdjustedCapitalStrategy = new CapitalStrategy();
+            var outstanding = 0.0;
+            Loan termLoan2 = Loan.CreateTermLoan(
+                riskAdjustedCapitalStrategy,
+                commitment,
+                outstanding,
+                riskRating,
+                maturity,
+                null);
+
+            Loan revolver = Loan.CreateRevolver(commitment, outstanding, riskRating, maturity, null);
+        }
+
         public class Loan
         {
             private double _commitment;
@@ -22,28 +42,12 @@ namespace RefactoringToPatternsPractice.Creation.ReplaceConstructorsWithCreation
 
             private CapitalStrategy _capitalStrategy;
 
-            public Loan(double commitment, int riskRating, DateTime maturity) : this(commitment, 0.00, riskRating, maturity, null)
-            {
-            }
-
-            public Loan(double commitment, int riskRating, DateTime maturity, DateTime expiry) : this(commitment, 0.00, riskRating, maturity, expiry)
-            {
-            }
-
-            public Loan(double commitment, double outstanding, int riskRating, DateTime maturity, DateTime? expiry) : this(null, commitment, outstanding, riskRating, maturity, expiry)
-            {
-            }
-
-            public Loan(CapitalStrategy capitalStrategy, double commitment, int riskRating, DateTime maturity, DateTime? expiry) : this(capitalStrategy, commitment, 0.00, riskRating, maturity, expiry)
-            {
-            }
-
-            public Loan(CapitalStrategy capitalStrategy, double commitment, double outstanding, int riskRating, DateTime maturity, DateTime? expiry)
+            public Loan(CapitalStrategy capitalStrategy, double commitment, double outstanding, int riskRating, DateTime? maturity, DateTime? expiry)
             {
                 this._commitment = commitment;
                 this._outstanding = outstanding;
                 this._riskRating = riskRating;
-                this._maturity = maturity;
+                this._maturity = maturity.Value;
                 this._expiry = expiry.Value;
                 this._capitalStrategy = capitalStrategy;
                 if (capitalStrategy == null)
@@ -62,22 +66,72 @@ namespace RefactoringToPatternsPractice.Creation.ReplaceConstructorsWithCreation
                     }
                 }
             }
+
+            public static Loan CreateTermLoan(double commitment, int riskRating, DateTime maturity)
+            {
+                return new Loan(null, commitment, 0.00, riskRating, maturity, null);
+            }
+
+            public static Loan CreateTermLoan(CapitalStrategy riskAdjustedCapitalStrategy, double commitment, double outstanding, int riskRating, DateTime maturity, DateTime? expiry)
+            {
+                return new Loan(riskAdjustedCapitalStrategy, commitment, outstanding, riskRating, maturity, null);
+            }
+
+            public static Loan CreateRevolver(
+                double commitment,
+                double outstanding,
+                int riskRating,
+                DateTime maturity,
+                DateTime? expiry)
+            {
+                return new Loan(null, commitment, outstanding, riskRating, maturity, expiry);
+            }
+
+            public static Loan CreateRevolver(
+                CapitalStrategy riskAdjustedCapitalStrategy,
+                double commitment,
+                double outstanding,
+                int riskRating,
+                DateTime? expiry)
+            {
+                return new Loan(riskAdjustedCapitalStrategy, commitment, outstanding, riskRating, null, expiry);
+            }
+
+            public static Loan CreateRCTL(
+                double commitment,
+                double outstanding,
+                int riskRating,
+                DateTime maturity,
+                DateTime expiry)
+            {
+                return new Loan(null, commitment, outstanding, riskRating, maturity, expiry);
+            }
+
+            public static Loan CreateRCTL(
+                CapitalStrategy riskAdjustedCapitalStrategy,
+                double outstanding,
+                int riskRating,
+                DateTime maturity,
+                DateTime expiry)
+            {
+                return new Loan(riskAdjustedCapitalStrategy, outstanding, outstanding, riskRating, maturity, expiry);
+            }
         }
-    }
 
-    public class CapitalStrategyRCTL : CapitalStrategy
-    {
-    }
+        public class CapitalStrategyRCTL : CapitalStrategy
+        {
+        }
 
-    public class CapitalStrategyRevolver : CapitalStrategy
-    {
-    }
+        public class CapitalStrategyRevolver : CapitalStrategy
+        {
+        }
 
-    public class CapitalStrategyTermLoan : CapitalStrategy
-    {
-    }
+        public class CapitalStrategyTermLoan : CapitalStrategy
+        {
+        }
 
-    public class CapitalStrategy
-    {
+        public class CapitalStrategy
+        {
+        }
     }
 }
